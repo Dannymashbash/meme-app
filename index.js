ab1289a40dacdd0ee4d10ab7aaa2cff4ab729b00
Mpho Luruli <91409809+Dannymashbash@users.noreply.github.com>
2026-06-10 17:05:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -170,13 +170,18 @@ const catsData = [
 ]
 
 function getEmotionsArray(cats){
-    const emotionsArray = []
-    for (let i = 0; i < cats.length; i++){
-        for (let j=0; j < cats[i].emotionTags.length; j++){
-                emotionsArray.push(cats[i].emotionTags[j])
-        }
-    }
-console.log(emotionsArray)
+   const catEmotion = []
+   for(let cat of cats){
+     for( let emotions of cat.emotionTags){
+        catEmotion.push(emotions)
+     }
+   }
+   return catEmotion
 }
 
+function renderEmotionsRadios(cats){
+ const emotions = getEmotionsArray(cats)
+ console.log(emotions)
+}
 
+renderEmotionsRadios(catsData)
